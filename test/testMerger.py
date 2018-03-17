d47f9f189d7878a9800d1667bd18d7cc097b3606
Tevin Achong <tevinachong@yahoo.com>
2018-03-17 13:41:03 -0400

    Editing URLs

--- test/testMerger.py
+++ test/testMerger.py
@@ -1,15 +1,17 @@
-from section5 import *
+import sys
+sys.path.insert(0, '../py')
+import merger
 
 var1 = ['Bat', 'Wonder', 'Iron', 'Bruce', 'Super']; 
 var2 = ['man', 'Woman', 'Man', 'Banner', 'Man'];
 
 def test_mergeHandler():
-    return mergeHandler(var1, var2, merge)
+    return merger.mergeHandler(var1, var2, merger.merge)
 print (test_mergeHandler())
 
 
 def test_merge2Single():
-    return merge2Single(var1, var2)
+    return merger.merge2Single(var1, var2)
 print (test_merge2Single())
 
 '''
